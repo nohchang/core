1b1fdc6ef8c1b53bb25a0b85f7b44e4b89145164
nohchang <nchq2123@naver.com>
2023-04-27 19:27:58 +0900

    끝

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,17 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+
+    }
+}
